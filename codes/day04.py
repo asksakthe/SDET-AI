06fad5756c4dfc9bdf4134f6f4a5a39aa2a42ac3
sakthee <asksakthe@outlook.com>
2026-05-25 23:14:58 +0530

    day 04 push

---
+++ codes/day04.py
@@ -0,0 +1,34 @@
+import day02 # Assuming day02.py contains some relevant code we want to use
+
+            
+def safe_analyse(documents):
+    try:
+        if isinstance(documents, list):
+            results = []
+            for i, ind in enumerate(documents):
+                if isinstance(ind, str):
+                    result = {'text': ind, 'Words': day02.get_word_count(ind), 'Chars': day02.get_char_count(ind), 'Upper': day02.make_upper(ind)}
+                    results.append(result)
+                else:
+                    print(f"Warning: Document {i+1} is not a string and will be skipped.")
+            return results
+        else:
+            raise ValueError("Input is not a list of documents.")
+    except Exception as e:
+        print(f"Error: {e}")
+        
+
+doc_Normal = ["AI is powerful", "Python is great"]
+doc_empty = []
+doc_mixed = ["Valid sentence", 12345, None, "Another valid one"]
+doc_notList = "I forgot to put this in a list"
+doc_collection = [doc_Normal, doc_empty, doc_mixed, doc_notList]
+for i, ind in enumerate(doc_collection):
+    print(f"Analyzing Document Set {i+1}:")
+    result = safe_analyse(ind)
+    if result is not None:
+        for j, res in enumerate(result):
+            print(f"\tDocument {j+1}:\n\t\t Text: {res['text']}\n\t\t Word Count: {res['Words']}\n\t\t Character Count: {res['Chars']}\n\t\t Uppercase: {res['Upper']}\n")
+
+            
+
