2a2b0c8535d57234f4e8198e25f3ab11d0b67d11
sakthee <asksakthe@outlook.com>
2026-06-05 08:09:05 +0530

    Day 06 changes merge

--- codes/phase01/day06_file_handler.py
+++ codes/phase01/day06_file_handler.py
@@ -1,18 +1,62 @@
+import os
+
 
 def read_file(file_path):
-    with open(file_path, 'r') as foo:
-        content = foo.read()
-        return content
+    try:
+        with open(file_path, 'r') as foo:
+            content = foo.read()
+            return content
+    except FileNotFoundError as e:
+        return F"getting {e} for file {file_path} "
+# Function 2 — read file, return as list of lines (no empty lines)
+def read_lines(filepath):
+    with open(filepath, 'r') as foo:
+        lines = foo.readlines()
+        doc_list = []
+        for i in lines:
+            doc_list.append(i)
+        if len(doc_list) == 0:
+            return f"The file {filepath} has no contents, please check"
+        else:
+            return len(doc_list)
+            
 
-def read_lines(file_path):
-    with open(file_path, 'r') as foo:
-        lines = file_path.readlines()
+def read_as_documents(filepath):
+    with open(filepath, 'r') as foo:
+        file_content = foo
+        result = []
+        for i, cont in enumerate(file_content):
+            dic = {"line Num": i+1, "text": cont.strip()}
+            result.append(dic)
+        return result
 
-    for i, content in enumerate(lines):
-        print(f"Line_num {i+1}: content")
+def safe_read_file(filepath):
+    try:
+        with open(filepath) as foo:
+            fileContents = foo.read()
+            if not fileContents.strip():
+                return f"Error : {filepath} is empty"
+            return fileContents
+    except FileNotFoundError:
+        return f"The {filepath} file doesn't exist, Check once"
 
-    pass
+if __name__ == '__main__':
+    # Get the directory where this script is located
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, "newFile.txt")
+    code_funct = safe_read_file(filepath=file_path)
+    print(f"output of safe_read_file and length of safe_read_file function: {len(code_funct)}")
+    print(f"First letter of safe_read_file function: {code_funct[0]}")
+    print("*****************************")
+    code01 = read_file(file_path)
+    print(f"First letter of read_file function: {code01[0]}")
+    print("*****************************")
+    code02 = read_lines(file_path)
+    print(f"output of read_lines function: {code02}")
+    print("*****************************")
+    code03 = read_as_documents(filepath=file_path)
+    print(f"First letter of read_as_documents function: {code03[0]}")
+    print("*****************************")
 
-def read_li():
-    
+
 
